02997b1f3c19e85bc1d2a33d8d79f48bf43a7d4c
Geon Park <101086624+GGeeoonn@users.noreply.github.com>
2025-05-26 10:33:06 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="RaDL",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="Geonpark",
+    author_email="geonpark@korea.ac.kr",
+    description="RaDL: Relation-aware Disentangled Learning for Multi-Instance Text-to-Image Generation",
+    url="https://github.com/ggeeoonn/RaDL",
+)
